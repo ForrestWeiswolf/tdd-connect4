4e985d7ef17b259145b1d2c66461af231152ccae
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-03-23 17:16:59 -0400

    game keeps track of current player; created game.turn and tests; renamed playerY to playerO

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,22 @@
 require "board"
+require "player"
 
 class Game
-	attr_reader :board, :players
-	def initialize
+	attr_reader :board, :players, :current
+	def initialize(players = [Player.new("X"), Player.new("O")])
 		@board = Board.new()
-		@players = ["X", "O"]
+		@players = players
+		@current = @players[0]
+	end
+
+	def turn
+		move = @current.move
+		if board.invalid_move(move)
+			puts "That column is full."
+			self.turn
+		else
+			board.play(move, @current.token)
+			#@current = players[players.find(@current)+1]
+		end
 	end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -6,21 +6,26 @@ describe Game do
 		expect(game.board.class).to eql(Board)
 	end
 	it 'has players' do
-		expect(game.players.class).to eql(Array)
+		expect(game.players[0].class).to eql(Player)
 	end
 
-	# describe ".play" do
-	# 	before do
-	# 		playerX = double
-	# 		playerY = double
-	# 		allow(playerX).to receive(:play).and_return('1')
-	# 		allow(playerY).to receive(:play).and_return('2')
-	# 	end
+	it 'keeps track of the current player' do
+		expect(game.current.class).to eql(Player)
+	end
 
-	# 	context "when the game starts" do 
-	# 		it "asks the first player for a move" do
-	# 			expect(playerX).to receive(:play)
-	# 		end
-	# 	end
-	# end
+	describe ".turn" do
+
+		it "asks the current player for a move" do
+			playerX = double
+			playerO = double
+			allow(playerX).to receive(:move).and_return(1)
+			allow(playerX).to receive(:token).and_return("X")
+			allow(playerO).to receive(:move).and_return(2)
+			allow(playerO).to receive(:token).and_return("O")
+			game_with_mocks = Game.new([playerX, playerO])
+
+			expect(game_with_mocks.current).to receive(:move)
+			game_with_mocks.turn
+		end
+	end
 end
