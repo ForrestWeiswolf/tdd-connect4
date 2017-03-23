5bbcc157a3dc2b6554ad4d17848aed35048ac758
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-03-23 11:05:19 -0400

    Player has a token and a move method

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,30 @@
-# class Player
-# 	attr_reader :token
-# 	def initialize(token)
-# 		@token = token
-# 	end
+class Player
+	attr_reader :token
+	def initialize(token)
+		@token = token
+	end
 
-# 	def move(board)
-# 		puts board
-		
-# end
+	def move(board)
+		puts board
+		puts "Where would you like to drop a piece, player #{@token}?"
+		move = parse_move(gets.chomp)
+		return move	
+	end
+
+	def parse_move(input)
+		begin
+			col = input[(/((\d)+)$/)]
+
+			unless col
+				raise "Exception"
+			end
+
+			col = col.to_i - 1
+		rescue
+			puts "Please input the number of the column you want to play in"
+			return self.move
+		else
+			return col
+		end
+	end
+end
