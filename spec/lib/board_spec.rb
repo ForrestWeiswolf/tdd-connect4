ef8aa636bdf39bee5e84bf4f3b2b58c09eaccae9
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2017-04-07 09:57:38 -0400

    Revised spec for Board.play for a 7*6 board

--- spec/lib/board_spec.rb
+++ spec/lib/board_spec.rb
@@ -8,17 +8,17 @@ describe Board do
 		expect(board.grid[6].size).to eql(6)
 	end
 
-	# describe ".play" do
-	# 	it "puts a token on the bottom row of an empty board" do
-	# 		board.play(0, "X")
-	# 		expect(board.grid[3][0]).to eql("X")
-	# 	end
-	# 	it "puts a token above all of the filled space in a column" do
-	# 		board.play(0, "X")
-	# 		board.play(0, "O")
-	# 		expect(board.grid[2][0]).to eql("O")
-	# 	end
-	# end
+	describe ".play" do
+		it "puts a token on the bottom row of an empty board" do
+			board.play(0, "X")
+			expect(board.grid[6][0]).to eql("X")
+		end
+		it "puts a token above all of the filled space in a column" do
+			board.play(0, "X")
+			board.play(0, "O")
+			expect(board.grid[5][0]).to eql("O")
+		end
+	end
 
 	# describe ".winner" do 
 	# 	it "returns false when neither player has won" do
